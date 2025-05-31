50138ca3a8cc1853f2d297fdaaaab8d046edb0d6
phonkee <phonkee@phonkee.eu>
2025-06-01 01:43:48 +0200

    updates

--- src/view/detail/view.rs
+++ src/view/detail/view.rs
@@ -1,14 +1,19 @@
 use super::lookup::Lookup;
 use crate::Error;
 use crate::view::View;
+use crate::view::list::ListView;
 use axum::Router;
 use axum::http::Method;
 use axum::http::request::Parts;
+use axum::response::{IntoResponse, Response};
+use axum::routing::{MethodFilter, on};
 use sea_orm::EntityTrait;
 use std::marker::PhantomData;
 use std::pin::Pin;
+use tracing::debug;
 
 /// DetailView is a view for displaying details of a single entity.
+#[derive(Clone)]
 pub struct DetailView<M, S, O>
 where
     M: EntityTrait,
@@ -38,6 +43,7 @@ where
     }
 }
 
+/// Implementing View for DetailView to render the detail view.
 impl<M, S, O> View<S> for DetailView<M, S, O>
 where
     M: EntityTrait,
@@ -71,8 +77,57 @@ where
     fn register_router_with_prefix(
         &self,
         router: Router<S>,
-        _prefix: &str,
+        prefix: &str,
     ) -> Result<Router<S>, Error> {
-        Ok(router)
+        let mf: MethodFilter = self.method.clone().try_into().map_err(|e| {
+            Error::InvalidMethod(format!(
+                "Failed to convert method {} to MethodFilter: {}",
+                self.method, e
+            ))
+        })?;
+
+        debug!(
+            "list view: {}{}, method: {}",
+            prefix, self.path, self.method
+        );
+
+        // Register the ListView with the axum router
+        Ok(router.route(self.path.clone().as_str(), on(mf, self.clone())))
+    }
+}
+
+impl<M, S, O> axum::handler::Handler<(), S> for DetailView<M, S, O>
+where
+    M: EntityTrait,
+    <M as EntityTrait>::Model: Into<O>,
+    S: Clone + Send + Sync + 'static,
+    O: serde::Serialize + Clone + Sync + Send + 'static,
+{
+    // Future type for the handler
+    type Future = Pin<Box<dyn Future<Output = Response> + Send + Sync + 'static>>;
+
+    // Call method to handle the request
+    #[allow(unused_variables)]
+    fn call(self, _req: axum::extract::Request, _state: S) -> Self::Future {
+        let (mut parts, body) = _req.into_parts();
+
+        let state = _state.clone();
+
+        Box::pin(async move {
+            match self.view(&mut parts, state).await {
+                Ok(value) => {
+                    // Convert the value to a JSON response
+                    (
+                        axum::http::StatusCode::OK,
+                        serde_json::to_string(&value).unwrap(),
+                    )
+                        .into_response()
+                }
+                Err(e) => {
+                    // Handle error and return a 500 Internal Server Error
+                    (axum::http::StatusCode::INTERNAL_SERVER_ERROR, e.to_string()).into_response()
+                }
+            }
+        })
     }
 }
--- src/view/mod.rs
+++ src/view/mod.rs
@@ -5,8 +5,11 @@ pub mod list;
 pub mod when;
 
 use crate::router::RouterExt;
+use crate::view::detail::DetailView;
+use axum::extract::Request;
 use axum::http::request::Parts;
 pub use error::Error;
+use std::pin::Pin;
 
 /// View trait for defining a view (List, Get, Delete, Update, Create)
 /// TODO: This trait is still in development and may change in the future.
