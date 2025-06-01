ef513f6fafdd27a2a81366c46b9748b7c448affd
phonkee <phonkee@phonkee.eu>
2025-06-01 02:49:32 +0200

    updates

--- src/view/list/mod.rs
+++ src/view/list/mod.rs
@@ -1,15 +1,3 @@
 pub mod view;
 
 pub use view::ListView;
-
-pub trait ListViewTrait {
-    /// list method to retrieve a list of items
-    fn list<S, O>(
-        &self,
-        req: axum::extract::Request,
-        state: S,
-    ) -> Result<Vec<O>, crate::error::Error>
-    where
-        S: Clone + Send + Sync + 'static,
-        O: serde::Serialize + Clone + Send + Sync + 'static;
-}
