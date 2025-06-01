aeb8fe209db3baf1752529527669d7eede2264c8
phonkee <phonkee@phonkee.eu>
2025-06-01 04:40:01 +0200

    updates

--- src/view/when/when.rs
+++ src/view/when/when.rs
@@ -17,7 +17,7 @@ where
 }
 
 /// When implementation for a function that takes parts and state and returns a future
-impl<S, F, R> When<S, ((),)> for F
+impl<S, F, R> When<S, f32> for F
 where
     S: Clone + Send + Sync + 'static,
     R: Future<Output = Result<(), super::error::Error>> + Send + Sync + 'static,
@@ -36,45 +36,45 @@ where
     }
 }
 
-// macro_rules! impl_when_func {
-//     ([$($ty:ident),*]) => {
-//         #[allow(non_snake_case)]
-//         impl<S, F, R, $($ty,)*> When<S, ($($ty,)*)> for F
-//         where
-//             S: Clone + Send + Sync + 'static,
-//             R: Future<Output = Result<(), super::error::Error>> + Send + Sync + 'static,
-//             F: Fn(&mut Parts, S, $($ty,)*) -> R + Send + Sync + 'static,
-//             $(
-//                 $ty: FromRequestParts<S> + Send + 'static,
-//             )*
-//         {
-//             type Future = Pin<Box<dyn Future<Output = Result<(), super::error::Error>> + Send + 'static>>;
-//
-//             fn when(self, parts: &mut Parts, state: S) -> Self::Future {
-//                 let mut parts = parts.clone();
-//                 let state = state.clone();
-//
-//                 Box::pin(async move {
-//                     $(
-//                         // create T1 from request parts
-//                         let $ty = $ty::from_request_parts(&mut parts, &state)
-//                             .await
-//                             .map_err(|_| super::error::Error::NoMatch)?;
-//                     )*
-//
-//                     self(&mut parts, state.clone(), $($ty,)*).await
-//                 })
-//             }
-//         }
-//     }
-// }
-//
-// // implement When for functions with 1 to 8 parameters
-// impl_when_func!([T1]);
-// impl_when_func!([T1, T2]);
-// impl_when_func!([T1, T2, T3]);
-// impl_when_func!([T1, T2, T3, T4]);
-// impl_when_func!([T1, T2, T3, T4, T5]);
-// impl_when_func!([T1, T2, T3, T4, T5, T6]);
-// impl_when_func!([T1, T2, T3, T4, T5, T6, T7]);
-// impl_when_func!([T1, T2, T3, T4, T5, T6, T7, T8]);
+macro_rules! impl_when_func {
+    ([$($ty:ident),*]) => {
+        #[allow(non_snake_case)]
+        impl<S, F, R, $($ty,)*> When<S, ($($ty,)*)> for F
+        where
+            S: Clone + Send + Sync + 'static,
+            R: Future<Output = Result<(), super::error::Error>> + Send + Sync + 'static,
+            F: Fn(Parts, S, $($ty,)*) -> R + Send + Sync + 'static,
+            $(
+                $ty: FromRequestParts<S> + Send + 'static,
+            )*
+        {
+            type Future = Pin<Box<dyn Future<Output = Result<(), super::error::Error>> + Send + 'static>>;
+
+            fn when(self, parts: Parts, state: S) -> Self::Future {
+                let mut parts = parts.clone();
+                let state = state.clone();
+
+                Box::pin(async move {
+                    $(
+                        // create T1 from request parts
+                        let $ty = $ty::from_request_parts(&mut parts, &state)
+                            .await
+                            .map_err(|_| super::error::Error::NoMatch)?;
+                    )*
+
+                    self(parts, state.clone(), $($ty,)*).await
+                })
+            }
+        }
+    }
+}
+
+// implement When for functions with 1 to 8 parameters
+impl_when_func!([T1]);
+impl_when_func!([T1, T2]);
+impl_when_func!([T1, T2, T3]);
+impl_when_func!([T1, T2, T3, T4]);
+impl_when_func!([T1, T2, T3, T4, T5]);
+impl_when_func!([T1, T2, T3, T4, T5, T6]);
+impl_when_func!([T1, T2, T3, T4, T5, T6, T7]);
+impl_when_func!([T1, T2, T3, T4, T5, T6, T7, T8]);
