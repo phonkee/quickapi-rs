1f947baab30b8d67dd434752ab2ac68be94593c7
phonkee <phonkee@phonkee.eu>
2025-06-15 05:54:44 +0200

    updates

--- src/quickapi.rs
+++ src/quickapi.rs
@@ -1,9 +1,5 @@
-use crate::Error;
-use sea_orm::DatabaseConnection;
-
-
 /// Create a new instance of QuickApi with the provided database connection.
-pub fn new<S>(db: DatabaseConnection) -> QuickApi<S> {
+pub fn new<S>(db: sea_orm::DatabaseConnection) -> QuickApi<S> {
     QuickApi::<S> {
         db,
         _marker: std::marker::PhantomData,
@@ -12,8 +8,8 @@ pub fn new<S>(db: DatabaseConnection) -> QuickApi<S> {
 
 /// QuickApi is the main entry point for the QuickAPI framework, providing a database connection
 #[derive(Debug)]
+#[allow(dead_code)]
 pub struct QuickApi<S> {
     pub(crate) db: sea_orm::DatabaseConnection,
     pub(crate) _marker: std::marker::PhantomData<S>,
 }
-
